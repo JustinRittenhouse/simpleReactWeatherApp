7fac18be3f1d64001cbd85d2d57260a498b9854f
Justin Rittenhouse <rittenhouse.justin@gmail.com>
2022-04-13 16:36:16 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Routes, Link } from 'react-router-dom'
+import { Home } from './views/Home'
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <React.Fragment>
+      <header>
 
-export default App;
+      </header>
+      <main>
+        <Routes>
+          <Route exact path='/' element={ <Home /> }/>
+        </Routes>
+      </main>
+      <footer>
+
+      </footer>
+    </React.Fragment>
+  )
+}
